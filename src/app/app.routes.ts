c078d6613132b707a87920187b6accbfa381266e
NourKanout <mnkanout@gmail.com>
2024-12-23 23:03:25 +0100

    ionic generate page about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then( m => m.HomePage)
   },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.page').then( m => m.AboutPage)
+  },
 ];
