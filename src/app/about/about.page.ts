323f70e1e4d5a99bd5cc42be0660fbc9b9ea378e
NourKanout <mnkanout@gmail.com>
2024-12-24 00:41:52 +0100

    Added the contact list icons

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,18 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonListHeader,
+  IonLabel,
+  IonItem,
+  IonIcon,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonCard,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol
+ } from '@ionic/angular/standalone';
+ import { addIcons } from 'ionicons';
+
+ import { 
+  call,
+  mailOpen,
+  logoGithub,
+  logoLinkedin,
+ } from 'ionicons/icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonImg,
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonListHeader,
+    IonList,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ]
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ 
+      call,
+      mailOpen,
+      logoGithub,
+      logoLinkedin,
+     });
+  }
 
   ngOnInit() {
   }
