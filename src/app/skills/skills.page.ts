1cdc52463f727fe1fe9ba695c9d495238c73a21b
NourKanout <mnkanout@gmail.com>
2024-12-24 02:52:07 +0100

    Added the skills page content

--- src/app/skills/skills.page.ts
+++ src/app/skills/skills.page.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonList,
+  IonListHeader,
+  IonLabel,
+  IonItem,
+ } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.page.html',
   styleUrls: ['./skills.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonItem,
+    IonLabel,
+    IonListHeader,
+    IonList,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ]
 })
 export class SkillsPage implements OnInit {
 
